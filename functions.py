170d5d279d4af2e08e7b6fca4a62ec3c21def665
Dohl9k <ze22ro@gmail.com>
2023-05-06 20:36:17 +0300

    add functions

--- functions.py
+++ functions.py
@@ -1,9 +1,35 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
 labels_dict = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,'L':11,'M':12,
                    'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,'V':21,'W':22,'X':23,'Y':24,
                    'Z':25}
+
+classifier = tf.keras.models.load_model('asl_predictor.h5')
+
 
 def get_label(prediction): 
     for ins in labels_dict:
         if prediction == labels_dict[ins]:
             return ins
 
+
+def prepare_img(img):
+    size_img = 64,64
+    try:
+        img = cv2.resize(img, size_img)
+    except:
+        return None
+    img = np.array(img)
+    img = img.astype('float32')/255.0
+    return img
+
+def get_sign(img):
+    try:
+        prediction = classifier.predict(img.reshape(1,64,64,3), verbose=0)[0]
+        prediction = np.argmax(prediction)
+    except:
+        return None
+    sign = get_label(prediction)
+    return sign
--- main.py
+++ main.py
@@ -1,68 +1,47 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
 import functions as f
-import tensorflow as tf
 import numpy as np
 from collections import Counter
+import tensorflow as tf
 
 offset = 100
 size_img = 64,64
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
-classifier = tf.keras.models.load_model('asl_predictor.h5')
-gestures = []
+# classifier = tf.keras.models.load_model('asl_predictor.h5')
+signs = []
 
 
 while True:
-    # Get image frame
     success, img = cap.read()
-    # Find the hand and its landmarks
-    hands = detector.findHands(img, draw=False)  # with draw
-    # hands = detector.findHands(img, draw=False)  # without draw
+    hands = detector.findHands(img, draw=False) 
 
     if hands:
-        # Hand 1
         hand = hands[0]
-        bbox = hand["bbox"]  # Bounding box info x,y,w,h
+        bbox = hand["bbox"] 
         x, y, w, h = hand['bbox']  
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
-        try:
-            temp = cv2.resize(imgCrop, size_img)
-        except:
+        temp = f.prepare_img(imgCrop)
+        if type(temp) != np.ndarray:
             continue
-        # images = []
-        # images.append(temp)
-        # images = np.array(images)
-        # images = images.astype('float32')/255.0
-        # image = images[0]
-        temp = np.array(temp)
-        temp = temp.astype('float32')/255.0
-        prediction = classifier.predict(temp.reshape(1,64,64,3))[0]
-        prediction = np.argmax(prediction)
-        gest = f.get_label(prediction)
-        gestures.append(gest)
+        sign = f.get_sign(temp)
+        signs.append(sign)
         cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
                                   (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
                                   (255, 0, 255), 2)
-        cv2.putText(img, gest, (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+        cv2.putText(img, sign, (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
                                 2, (255, 0, 255), 2)      
         
         cv2.imshow("ImageCrop", imgCrop)
         
-        if len(gestures) == 30:
-            count_gestures = Counter(gestures)
-            print(count_gestures)
-            # gest_max = list(count_gestures.keys())
-            # print(gest_max)
-            # tmp = count_gestures[gest_max[0]]
-            # print(tmp)
-            gest_max = [k for k, v in sorted(count_gestures.items(),
+        if len(signs) == 30:
+            count_signs = Counter(signs)
+            gest_max = [k for k, v in sorted(count_signs.items(),
                                               key=lambda item: item[1], reverse=True)]
-            print(f"List {gest_max}")
-            tmp = count_gestures[gest_max[0]]
-            print(f"Answer {tmp}")
-            print(f"Prediction:{tmp/len(gestures)}")
+            tmp = count_signs[gest_max[0]]
+            print(f"Prediction:{tmp/len(signs)}")
             break
 
     fps = int(cap.get(cv2.CAP_PROP_FPS))
